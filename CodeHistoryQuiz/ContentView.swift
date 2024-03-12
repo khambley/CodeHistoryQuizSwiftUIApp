22fd579a84b20825556acecef0c099b18df78d04
Katherine Hambley <superdesigngirl@mac.com>
2024-03-12 15:03:26 -0500

    Initial commit

--- CodeHistoryQuiz/ContentView.swift
+++ CodeHistoryQuiz/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
         }
-        .padding()
     }
 }
 
