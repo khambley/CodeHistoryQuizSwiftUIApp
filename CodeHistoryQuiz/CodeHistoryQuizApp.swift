f68822122b8afff7795865460ee70023506fd28a
Katherine Hambley <superdesigngirl@mac.com>
2024-03-12 14:49:05 -0500

    Initial Commit

---
+++ CodeHistoryQuiz/CodeHistoryQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryQuizApp.swift
+//  CodeHistoryQuiz
+//
+//  Created by Katherine Hambley on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
