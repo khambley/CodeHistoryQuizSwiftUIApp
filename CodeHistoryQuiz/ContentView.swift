c05e304b0eaed2d6a74a8bb6c8c555a76aa78eb1
Katherine Hambley <superdesigngirl@mac.com>
2024-03-12 20:35:41 -0500

    Added UI for question

--- CodeHistoryQuiz/ContentView.swift
+++ CodeHistoryQuiz/ContentView.swift
@@ -14,7 +14,61 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             mainColor.ignoresSafeArea()
+            VStack{
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                Spacer()
+                HStack{
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
         }
+        .foregroundColor(.white)
     }
 }
 
